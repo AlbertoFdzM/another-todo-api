69af03bb2168253d3531355e824a52785a0b59d9
Alberto Fernandez <Albertofdzm@gmail.com>
2018-05-09 16:02:40 +0200

    Create first test

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+const mocha = require('mocha')
+const describe = mocha.describe
+const it = mocha.it
+const assert = require('assert')
+
+describe('another-todo-api', () => {
+  it('should has tests', () => {
+    assert.ok('Everything is fine')
+  })
+})
